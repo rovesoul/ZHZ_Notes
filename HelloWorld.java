e41c36608a3aceb3447b3219ee9229e4a2b9957e
rovesoul <397703547@qq.com>
2020-05-12 09:50:48 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("asdfasdfasdf");
+		System.out.println("我是VS从的测试");
+	}
+}
--- just_test.py
+++ just_test.py
@@ -1,1 +1,9 @@
+import sklearn
+import matplotlib.pyplot as plt
+import random
+import re
 print(1)
+a = random.randint(2, 9)
+
+print(a)
+re.c
