b3f9fba411710668580380ed5821a5bd67e3a41c
rovesoul <397703547@qq.com>
2020-05-14 16:36:03 +0800

    无说明

--- just_test.py
+++ just_test.py
@@ -1,9 +1,15 @@
-import sklearn
-import matplotlib.pyplot as plt
-import random
-import re
-print(1)
-a = random.randint(2, 9)
+# import sklearn
+# import matplotlib.pyplot as plt
+# import random
+# import re
+import pandas as pd
 
-print(a)
-re.c
+df = pd.read_excel("2result.xlsx")
+
+def cut(content):
+    content=str(content).replace("\n","")
+    return content
+
+df['题干'] =df['题干'].apply(cut)
+print(df.shape)
+df.to_csv("清理后.csv")
